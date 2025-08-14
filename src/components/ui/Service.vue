5f3ca6dd22e710b10e13a43869c34d68ba40c0bb
MarcoVNDev <marcovndev@gmail.com>
2025-08-13 22:32:43 -0600

    fix: update Service component to use full width for better layout consistency

--- src/components/ui/Service.vue
+++ src/components/ui/Service.vue
@@ -8,7 +8,7 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <div :class="`flex flex-col items-start justify-end pl-2 pb-2 w-[300px] h-[200px] md:w-[250px] md:h-[350px] rounded-2xl ${props.class || ''}`">
+  <div :class="`flex flex-col items-start justify-end pl-2 pb-2 w-full h-[200px] md:w-[250px] md:h-[350px] rounded-2xl ${props.class || ''}`">
     <p class="text-2xl font-black text-white">{{ props.title }}</p>
   </div>
 </template>
